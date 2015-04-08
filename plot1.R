3789a5fa864a5709218e0d838340fa5d6314b2a6
tjj739 <tjarosz739@live.com>
2015-04-08 13:40:53 -0700

    Initial commit of plot1.R and plot1.png for Course Project 1 in Exploratory Data Analysis class.

---
+++ plot1.R
@@ -0,0 +1,20 @@
+## Loading the full data file
+allData <- read.csv("household_power_consumption.txt", header=TRUE
+                    ,sep=';', na.strings="?"
+                    )
+
+## Convert date field
+allData$Date <- as.Date(allData$Date, format="%d/%m/%Y")
+ 
+# ## Subset to data needed for this plot
+plotData <- subset(allData 
+                  ,(Date >= "2007/02/01" & Date <= "2007/02/02")
+                   )
+
+# Draw plot 1 with appropriate formatting
+hist(plotData$Global_active_power, main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+ 
+#  Saving plot to a  file
+dev.copy(png, file="plot1.png", height=480, width=480)
+dev.off()
